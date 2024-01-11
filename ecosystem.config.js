8ccc9df5dcd9187c21790f9224f62da375042e83
ardiansyah <ardiansyah@thinkspedia.id>
2024-01-11 08:57:35 +0700

    add ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: 'hris-api', //name PM2
+      script: 'dist/main.js', //llocation main.js on each project
+      watch: false,
+      env: {
+        DB_URI: 'mongodb://localhost/hris',
+        JWT_SECRET:
+          'eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJIcmlzIiwiVXNlcm5hbWUiOiJBcmRpYW5zeWFoIiwiZXhwIjoxNzA0ODc0MjMxLCJpYXQiOjE3MDQ4NzQyMzF9.dPYUlKHOAy5OPg5G6rdiH6UMvmc4q3rWXRhr5q6kGcQ',
+      },
+    },
+  ],
+};
