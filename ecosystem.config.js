5485a9d8f75a4bdca2d41da012661e4c49497616
ardiansyah <ardiansyah@thinkspedia.id>
2024-01-12 09:38:08 +0700

    update ecosystem pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       script: 'dist/main.js', //llocation main.js on each project
       watch: false,
       env: {
-        DB_URI: 'mongodb://localhost/hris',
+        DB_URI: 'mongodb://localhost:27017/hris',
         JWT_SECRET:
           'eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJIcmlzIiwiVXNlcm5hbWUiOiJBcmRpYW5zeWFoIiwiZXhwIjoxNzA0ODc0MjMxLCJpYXQiOjE3MDQ4NzQyMzF9.dPYUlKHOAy5OPg5G6rdiH6UMvmc4q3rWXRhr5q6kGcQ',
       },
